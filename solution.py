f64ef8cc81febb84a4202754b9c6c06cfc27ba31
Nhat Tan <lonhattan007@gmail.com>
2021-12-09 17:30:44 +0700

    solution.py

--- solution.py
+++ solution.py
@@ -20,46 +20,18 @@ def import_deck(filename: str) -> Deck:
 def solve(deck: Deck, goal: int):
     # """Solve the problem in Dynamic Programming style"""
     
-    # """Check for exceptions"""
-    # if deck.is_empty():
-    #     raise Exception("Deck is empty")
+    """Check for exceptions"""
+    if deck.is_empty():
+        raise Exception("Deck is empty")
     
-    # if len(deck.ranks) < 5:
-    #     raise Exception("Not enough ranks")  
+    if len(deck.ranks) < 5:
+        raise Exception("Not enough ranks")  
 
-    # if goal < deck.min_num() or goal > deck.max_num():
-    #     raise Exception("Goal number is out of deck range")
+    if goal < deck.min_num() or goal > deck.max_num():
+        raise Exception("Goal number is out of deck range")
 
-    # """Step 1: List all possible combos"""
-    # """Might check for different ranks as well"""
-    # solutions = []
-
-    # # TODO
-    # candidate_lst = deck.ranks
-    # goal_rank = deck.rank_of(goal)
-
-    # for rank_ind in range (0, len(candidate_lst)):
-    #     if rank_ind[0].rank != goal_rank:
-    #         pass  
-    #     else:
-    #         candidate_lst.pop(rank_ind)
-    #         break
-
+    # TODO
     
-    
-    # """Step 2: Check if their ranks are not the same"""
-    # """Might be deleted later"""
-    # if len(solutions) != 0:
-    #     unqualified = []
-
-    #     for sol in solutions:            
-    #         if sol[0].rank != sol[1].rank and sol[1].rank != sol[2].rank and sol[2].rank != sol[3].rank and sol[3].rank != sol[1].rank:
-    #             pass
-    #         else:
-    #             unqualified.append(sol)
-
-    #     for sol in unqualified:
-    #         solutions.remove(sol)
 
     return [] # solutions
 
