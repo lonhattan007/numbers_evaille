c34bb337ef2633c88aeae5b3c63c1cdca7d6caa0
NhatTan <lonhattan007@gmail.com>
2021-12-12 00:37:05 +0700

    Complete algorithm and program

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,8 @@
 import math
 
 def sum_of_n(sum, n, start):
+    """This function find all the combinations of n different number whose sum equals the given number"""
+    
     if n < 1:
         print("Invalid input: No of numbers must be at least 1!")
     elif sum < n*(n-1)/2:
@@ -9,22 +11,17 @@ def sum_of_n(sum, n, start):
         return [[sum]]
     else:
         result = []
-        
 
-        for i in range (start, sum-n+1):
-            sub_solution_lst = sum_of_n(sum-i, n-1, start+1)
+        for i in range (start, math.ceil(sum/n)):
+            sub_solution_lst = sum_of_n(sum-i, n-1, i+1)
             sub_solution_lst = list(filter(lambda a: len(a) == n-1, sub_solution_lst))
+            sub_solution_lst = list(filter(lambda a: i not in a, sub_solution_lst))
 
             if len(sub_solution_lst) == 0:
                 pass
             else:
                 for sub_solution in sub_solution_lst:
-                    sub_solution[-1] -= 1
                     sub_solution.insert(0, i)                
                     result.append(sub_solution)
         
         return result    
-
-solution = sum_of_n(10, 2, 1)
-
-print(solution)
--- solution.py
+++ solution.py
@@ -11,11 +11,14 @@ def import_card(string: str) -> Card:
 def import_deck(filename: str) -> Deck:
     deck = Deck()
     
-    with open(filename, "r") as open_file_obj:
-        for line in open_file_obj:
-            deck.sorted_append(import_card(line))
+    try:
+        with open(filename, "r") as open_file_obj:
+            for line in open_file_obj:
+                deck.sorted_append(import_card(line))
 
-    open_file_obj.close()
+        open_file_obj.close()
+    except:
+        print("Problem opening file!")
 
     return deck
 
@@ -38,63 +41,61 @@ def solve(deck: Deck, goal: int):
     """Select all the candidates: differs from the goal number in rank and smaller than goal - 5"""
     goal_rank = deck.rank_of(goal)
 
-    candidate_lst = list(filter(lambda a: a[0].rank != goal_rank, deck.rank_lst))
-    
-    i = 0
-    for candidate in candidate_lst:
-        candidate_lst[i] = list(filter(lambda a: a.number < goal - 6, candidate))
-        
-        if len(candidate_lst[i]) == 0:
-            candidate_lst.pop(i)
+    def check_valid_combo(lst) -> bool:
+        rank_lst = [deck.rank_of(n) for n in lst]
+
+        if goal_rank in rank_lst:
+            return False
+        elif len(rank_lst) != len(set(rank_lst)):
+            return False        
         else:
-            i+=1
+            return True
 
-    # solution_lst = []
+    solution_lst = sum_of_n(goal, 4, 1)
+    solution_lst = [n for n in solution_lst if check_valid_combo(n)]
 
-    # def foo(sum, parts, start, solution) -> any:
-    #     if parts == 1:
-    #         return [sum]
-    #     else:
-    #         for i in range(start, sum - parts*(parts-1)/2):
-    #             return solution + foo(sum-i, parts-1, i+1, solution)   
-    
-    # for i in range(1, goal - 6):
-    #     solution = []
-    #     left = goal - i
-
-
-
-
-    return candidate_lst
+    return solution_lst
 
 
 def main():
     deck = import_deck(argv[1])
-    deck.print()
 
     goal = int(input("Goal: "))
-
-    print("Rank of number", goal, "is:", deck.rank_of(goal))
-    print("Min number in the deck is:", deck.min_num())
-    print("Max number in the deck is:", deck.max_num())
 
     solutions = solve(deck, goal)
 
     """Print result"""
-    if len(solutions) == 0:
-        print("No solutions were found")
-    else:
-        no = 1
-        for sol in solutions:
-            print("Solution", no)
-            
-            for card in sol:
-                card.print()
+    try:
+        open_file_obj = open(argv[2], 'w')
 
-            print("_____________")            
-            
-            no += 1
+        if len(solutions) == 0:
+            msg = "No solutions were found\n"
+            print(msg, end="")
+            open_file_obj.write(msg)
+        else:
+            no = 1
+            for sol in solutions:
+                msg = "Solution " + str(no) + "\n"
+                open_file_obj.write(msg)
 
+                i = 1
+                for card_number in sol:
+                    if i != len(sol):
+                        msg = "Card " + str(card_number) + " - Rank " + str(deck.rank_of(card_number)) + "; "
+                    else:
+                        msg = "Card " + str(card_number) + " - Rank " + str(deck.rank_of(card_number)) + "\n"
+
+                    open_file_obj.write(msg)
+                    i+=1
+
+                open_file_obj.write("________________\n")                        
+                no += 1
+
+        open_file_obj.close()
+
+        print("All the solutions are in the output.txt file")
+    except:
+        print("Problem opening file!")
 
 if __name__ == "__main__":
     """Input: (1) list of cards, sorted in ascending order of card number
